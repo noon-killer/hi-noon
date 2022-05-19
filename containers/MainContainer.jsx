aad3f15d78e0151bdee8ec2326e7f4f586f8ec3a
Jin Oh <jintoh613@gmail.com>
2022-05-19 00:56:54 -0400

    fix paths

--- containers/MainContainer.jsx
+++ containers/MainContainer.jsx
@@ -3,11 +3,11 @@ import { TouchableOpacity } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { SignUp } from "../components/SignUpScreen";
-import { ChoiceScreen } from "../components/ChoiceScreen";
-import { ForumScreen } from "../components/ForumScreen";
-import { CardScreen } from "../components/CardScreen";
-import { PlayGroundScreen } from "../components/PlayGroundScreen";
+import { SignUp } from "../Components/SignUpScreen";
+import { ChoiceScreen } from "../Components/ChoiceScreen";
+import { ForumScreen } from "../Components/ForumScreen";
+import { CardScreen } from "../Components/CardScreen";
+import { PlayGroundScreen } from "../Components/PlayGroundScreen";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
